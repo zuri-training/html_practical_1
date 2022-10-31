c4812c3e688edc25b58db5002c72add8c4ea10fe
Gerald <info.gerald@gmail.com>
2022-10-31 06:13:19 -0400

    Java commit

--- java1.js
+++ java1.js
@@ -1,6 +1,1 @@
-if (z === 7) {
-    console.log("This is correct")
-} else if (z === 1000){
-    console.log("This is a big number")
-} else {
-    console.log("This isn\'t fun");
+let numb = prompt("Please enter a whole number");
